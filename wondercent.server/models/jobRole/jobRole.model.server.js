b9bbfd05da3a99eb6e32aeda9fddefbc57fc3e30
yuan95 <yuan95@ccs.neu.edu>
2016-06-24 17:52:37 -0400

    jobRole model

--- wondercent.server/models/jobRole/jobRole.model.server.js
+++ wondercent.server/models/jobRole/jobRole.model.server.js
@@ -1,17 +1,53 @@
 /**
  * Created by ChenxiYuan on 6/24/16.
  */
-module.exports = function(userModel) {
+module.exports = function (userModel) {
 
     var user = userModel.getMongooseModel();
 
     var api = {
-        createJobRole: createJobRole,
-        getJobRoleFromUser: getJobRoleFromUser,
+        addJobRole: addJobRole,
+        getJobRolesFromUser: getJobRolesFromUser,
         updateJobRole: updateJobRole,
         deleteJobRole: deleteJobRole
     };
 
     return api;
+
+    function addJobRole(jobRole) {
+        return user.jobRoles.push(jobRole);
+    }
+
+    function getJobRolesFromUser() {
+        return user.jobRoles;
+    }
+
+    function updateJobRole(jobRoleId, jobRole) {
+        var jobRoles = user.jobRoles;
+
+        for (var i in jobRoles) {
+            if (jobRoles[i]._id === jobRoleId) {
+                jobRoles.splice(i, 1);
+                jobRoles.splice(i, 0, jobRole);
+
+                // save to data base
+                user.save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function deleteJobRole(jobRoleId) {
+        for (var i in jobRoles) {
+            if (jobRoles[i]._id === jobRoleId) {
+                jobRoles.splice(i, 1);
+                // save to data base
+                user.save();
+                return true;
+            }
+        }
+        return false;
+    }
 
 };
