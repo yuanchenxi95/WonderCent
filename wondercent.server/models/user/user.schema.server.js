3ac7ca916116912db764095940f7d8ab7233f88d
BeanRider <weng.jef@husky.neu.edu>
2016-06-24 17:16:02 -0400

    Renamed and reformatted UserSchema

--- wondercent.server/models/user/user.schema.server.js
+++ wondercent.server/models/user/user.schema.server.js
@@ -1,16 +1,17 @@
 module.exports = function () {
     var mongoose = require("mongoose");
 
-    var profileSchema = require("./profile.schema.service")();
-    var jobRole = require("./jobRole.schema.server")();
+    var ProfileSchema = require("./profile.schema.service")();
+    var JobRoleSchema = require("./jobRole.schema.server")();
 
     var UserSchema = mongoose.Schema({
-        profile: profileSchema,
-        softDelete: {type: Boolean, default: false},
-        dateCreated: {type: Date, default: Date.now()},
-        jobRole: jobRole,
-        followingUsers: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        followerUsers: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
+        profile         : ProfileSchema,
+        softDelete      : {type: Boolean, default: false},
+        dateCreated     : {type: Date, default: Date.now()},
+        jobRoles        : [JobRoleSchema],
+
+        _followingUsers  : {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        _followerUsers   : {type: mongoose.Schema.Types.ObjectId, ref: "User"}
     }, { collection: "wondercent.user"});
     return UserSchema;
 };
