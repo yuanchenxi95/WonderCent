70b7af6197f54ba9844e77ce2d3deafdf713801a
BeanRider <weng.jef@husky.neu.edu>
2016-06-25 16:12:20 -0400

    Added register page and controller.

---
+++ public/views/user/register.controller.client.js
@@ -0,0 +1,48 @@
+(function() {
+    angular
+        .module("WonderCentApp")
+        .controller("RegisterController", RegisterController);
+    
+    function RegisterController($location, $rootScope, UserService) {
+        var vm = this;
+        vm.register = register;
+
+        function register(email, password, passwordVerify) {
+            // password validation
+            if (password !== passwordVerify) {
+                vm.error = "Passwords do not match!";
+                return;
+            }
+
+            var newUser = {};
+            newUser.email = email;
+            newUser.password = password;
+
+            UserService
+                .register(newUser)
+                .then(
+                    function(response) {
+                        var user = response.data;
+                        $rootScope.currentUser = user;
+                        $location.url("/user/" + user._id);
+                    },
+                    function(error) {
+                        vm.error = error.data;
+                    }
+                );
+
+            // Old way
+            // Unique email validation in the server side
+            // UserService
+            //     .createUser(newUser)
+            //     .then(
+            //         function(response) {
+            //             $location.url("/user/" + response.data._id);
+            //         },
+            //         function(error) {
+            //             vm.error = error.data;
+            //         }
+            //     );
+        }
+    }
+})();
