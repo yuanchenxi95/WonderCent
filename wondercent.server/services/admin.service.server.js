97943e60b3d1f26990b1008777e3d262ba9041f8
yuan95 <yuan95@ccs.neu.edu>
2016-06-25 21:34:11 -0400

    admin.service.server.js methods map finsihed

--- wondercent.server/services/admin.service.server.js
+++ wondercent.server/services/admin.service.server.js
@@ -1,11 +1,11 @@
 /**
  * Created by ChenxiYuan on 6/25/16.
  */
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require("bcrypt-nodejs");
-var FacebookStrategy = require('passport-facebook').Strategy;
-
+// var FacebookStrategy = require('passport-facebook').Strategy;
+//
 
 module.exports = function (app, models) {
     var adminModel = models.adminModel;
@@ -14,70 +14,31 @@ module.exports = function (app, models) {
     var jobRoleModel = models.jobRoleModel;
     var profileModel = models.profileModel;
 
-    // app.post("/api/login", passport.authenticate('wondercent'), login);
-    // app.get("/api/loggedin", loggedin);
-    // app.post("/api/register", register);
-    //
-    // app.post("/api/logout", authenticate, logout);
-    // // app.put("/api/user/email", authenticate, updateUserEmail);
-    // app.put("/api/user/password", authenticate, updateUserPassword);
-    // app.put("/api/user/following", authenticate, addFollowingUser);
-    // app.delete("/api/user", authenticate, deleteUser);
 
+    app.post("/api/admin/login", login);
+    // app.get("/api/admin/loggedin", loggedin);
+    app.post("/api/admin/register", register);
+    app.post("/api/admin/logout", logout);
+    app.put("/api/admin/password", updateAdminPassword);
+    app.delete("/api/admin/delete/:adminId", deleteAdmin);
 
-
-
-    passport.use('wondercentAdmin', new LocalStrategy(localStrategy));
-    passport.serializeUser(serializeUser);
-    passport.deserializeUser(deserializeUser);
-
-
-    function serializeUser(admin, done) {
-        done(null, admin);
+    function login(req, res) {
+        //TODO
     }
 
-    function authenticate(req, res, next) {
-        if (!req.isAuthenticated()) {
-            res.status(401).send("Not authenticated");
-        } else {
-            next();
-        }
+    function register(req, res) {
+        //TODO
     }
 
-    function deserializeUser(user, done) {
-        userModel
-            .findUserById(user._id)
-            .then(
-                function (user) {
-                    done(null, user);
-                },
-                function (err) {
-                    done(err, null);
-                }
-            );
+    function logout(req, res) {
+        // TODO
     }
 
-    function localStrategy(email, password, done) {
-        userModel
-            .findUserByEmail(email)
-            .then(
-                function (user) {
-                    if (!user) {
-                        return done(null, false);
-                    } else if (user && bcrypt.compareSync(password, user.password)) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false);
-                    }
-                },
-                function (err) {
-                    if (err) {
-                        return done(err);
-                    }
-                }
-            )
+    function updateAdminPassword(req, res) {
+        //TODO
     }
-
-
+    function deleteAdmin(req, res) {
+        //TODO
+    }
 };
 
